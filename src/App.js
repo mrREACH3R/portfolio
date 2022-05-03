9070cc630144d51a621924c4727310e942d9a469
shubham <shubhamsinghsolanki12345@gmail.com>
2022-05-03 23:43:29 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/navBar';
 
 function App() {
   return (
     <div className="App">
-     <h1>My name is shubham</h1>
-     <p>solanki</p>
+     <NavBar/>
     </div>
   );
 }
