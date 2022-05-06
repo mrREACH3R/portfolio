d0fc5ef9410f6ac8865101985426a5e01a7ec9a3
shubham <shubhamsinghsolanki12345@gmail.com>
2022-05-07 00:37:00 +0530

    adding content

---
+++ src/components/Experience/experience.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+// import './experience.css';
+
+const experience = ()=> {
+return(
+        <div className="experience">
+          <div className="achievement">
+            {/* darkMode */}
+            <div className="circle">3+</div>
+            <span>years </span>
+            <span>Experience</span>
+          </div>
+          <div className="achievement">
+            <div className="circle">10+</div>
+            <span>completed </span>
+            <span>Projects</span>
+          </div>
+          <div className="achievement">
+            <div className="circle">1+</div>
+            <span>companies </span>
+            <span>Work</span>
+          </div>
+        </div>
+      )
+
+
+}
+export default experience;
